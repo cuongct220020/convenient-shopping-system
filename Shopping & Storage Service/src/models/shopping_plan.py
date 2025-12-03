0a6869a1604ecc95ee7c85d19f833ce5029d3bcc
Thanh <151599889+ThanhIT0410@users.noreply.github.com>
2025-12-03 20:07:10 +0700

    feat(shopping-storage-service): initialize service with shopping plan and storage CRUD, consumption logic, and plan transition (#24)
    * feat: init Shopping & Storage Service
    
    * feat: models for Storage, StorableUnit, ShoppingPlan, Report
    
    * feat: Storage CRUD
    
    * feat: StorableUnit CRUD & consume
    
    * feat: StorableUnit CRUD & consume
    
    * refactor: change column name storable_units to storable_unit_list in Storage table
    
    * feat: add validator for fields requiring positive numbers
    
    * feat: add reserved_quantity to StorableUnit
    
    * feat: add plan transition (unfinished, waiting for merging)
    
    * feat: add plan transition (unfinished, waiting for merging)

---
+++ Shopping & Storage Service/src/models/shopping_plan.py
@@ -0,0 +1,36 @@
+
+from sqlalchemy import Integer, String, DateTime, ForeignKey, Enum, JSON
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from sqlalchemy.sql import func
+from datetime import datetime
+from enums.plan_status import PlanStatus
+from core.database import Base
+
+class ShoppingPlan(Base):
+    __tablename__ = "shopping_plans"
+
+    plan_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    group_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    last_modified: Mapped[datetime] = mapped_column(DateTime, default=func.now(), onupdate=func.now())
+    deadline: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    assigner_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    assignee_id: Mapped[int] = mapped_column(Integer, nullable=True)
+    shopping_list: Mapped[dict] = mapped_column(JSON, nullable=False)
+    plan_status: Mapped[PlanStatus] = mapped_column(Enum(PlanStatus), nullable=False, default=PlanStatus.CREATED)
+
+    report: Mapped["Report"] = relationship(back_populates="plan")
+
+class Report(Base):
+    __tablename__ = "reports"
+
+    report_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    plan_id: Mapped[int] = mapped_column(ForeignKey("shopping_plans.plan_id"), nullable=False, unique=True)
+    report_date: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=func.now())
+    report_content: Mapped[dict] = mapped_column(JSON, nullable=False)
+    spent_amount: Mapped[int] = mapped_column(Integer, default=0)
+
+    plan: Mapped["ShoppingPlan"] = relationship(
+        back_populates="report",
+        foreign_keys=[plan_id],
+        single_parent=True
+    )
---
+++ Shopping & Storage Service/src/models/storage.py
@@ -0,0 +1,71 @@
+from sqlalchemy import Integer, String, Float, DateTime, Enum, ForeignKey, CheckConstraint, event, update
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from sqlalchemy.sql import func
+from datetime import datetime
+from enums.uc_measurement_unit import UCMeasurementUnit
+from enums.storage_type import StorageType
+from core.database import Base
+
+class Storage(Base):
+    __tablename__ = "storages"
+
+    storage_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    storage_name: Mapped[str] = mapped_column(String)
+    storage_type: Mapped[StorageType] = mapped_column(Enum(StorageType), nullable=False)
+    group_id: Mapped[int] = mapped_column(Integer, nullable=False)
+
+    storage_unit_list: Mapped[list["StorableUnit"]] = relationship(
+        back_populates="storage",
+        cascade="all, delete-orphan"
+    )
+
+@event.listens_for(Storage, "after_insert")
+def set_storage_name_after_insert(mapper, connection, target):
+    if not target.storage_name:
+        new_name = f"{target.storage_type.name.replace('_', ' ').title()} #{target.storage_id} of Group #{target.group_id}"
+        connection.execute(
+            update(Storage)
+            .where(Storage.storage_id == target.storage_id)
+            .values(storage_name=new_name)
+        )
+        target.storage_name = new_name
+
+class StorableUnit(Base):
+    __tablename__ = "storable_units"
+
+    unit_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    storage_id: Mapped[int] = mapped_column(ForeignKey("storages.storage_id"), nullable=False)
+    package_quantity: Mapped[int] = mapped_column(Integer, default=1)
+    reserved_quantity: Mapped[int] = mapped_column(Integer, default=0)
+    unit_name: Mapped[str] = mapped_column(String, nullable=False)
+    component_id: Mapped[int] = mapped_column(Integer)
+    content_type: Mapped[str] = mapped_column(String)
+    content_quantity: Mapped[float] = mapped_column(Float)
+    content_unit: Mapped[UCMeasurementUnit] = mapped_column(Enum(UCMeasurementUnit))
+    added_date: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=func.now())
+    expiration_date: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=True)
+
+    storage: Mapped["Storage"] = relationship(
+        back_populates="storage_unit_list",
+        foreign_keys=[storage_id]
+    )
+
+    __table_args__ = (
+        CheckConstraint(
+            "(component_id IS NULL AND content_type IS NULL) "
+            "OR (component_id IS NOT NULL AND content_type IS NOT NULL)",
+            name="component_id_type_pairing"
+        ),
+        CheckConstraint(
+            "(content_type = 'countable_ingredient' AND "
+            " content_quantity IS NULL AND content_unit IS NULL) "
+            "OR "
+            "(content_type = 'uncountable_ingredient' AND "
+            " content_quantity IS NOT NULL AND content_unit IS NOT NULL)",
+            name="quantity_unit_required_for_measurable"
+        ),
+        CheckConstraint(
+            "reserved_quantity <= package_quantity",
+            name="reserved_le_package"
+        )
+    )
