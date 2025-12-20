0463ebfa6364eae4cc7e72ac1222cdb266b0c210
ThanhIT0410 <trinhthanhit104@gmail.com>
2025-12-20 11:04:01 +0700

    feat: init Meal Service main

---
+++ Meal Service/main.py
@@ -0,0 +1,24 @@
+from database import engine, Base
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from apis.v1.meal_api import meal_router
+
+app = FastAPI(
+    title="Meal Service",
+    description="API for managing meals",
+    version="0.1.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(meal_router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8003, reload=False, log_level="debug")
