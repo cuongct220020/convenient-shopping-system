4bde85eabc61a6d8ef9858f9ee85bd542d219852
ThanhIT0410 <trinhthanhit104@gmail.com>
2025-12-24 07:48:18 +0700

    feat: new stacked representation for StorableUnit

--- Shopping and Storage Service/src/apis/v1/storable_unit_api.py
+++ Shopping and Storage Service/src/apis/v1/storable_unit_api.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import inspect
 from typing import Optional
 from services.storable_unit_crud import StorableUnitCRUD
-from schemas.storable_unit_schemas import StorableUnitCreate, StorableUnitUpdate, StorableUnitResponse
+from schemas.storable_unit_schemas import StorableUnitCreate, StorableUnitUpdate, StorableUnitResponse, StorableUnitStackedResponse
 from models.storage import StorableUnit
 from shared.shopping_shared.schemas.response_schema import GenericResponse, PaginationResponse
 from database import get_db
@@ -18,12 +18,12 @@ storable_unit_router = APIRouter(
 
 @storable_unit_router.get(
     "/{id}",
-    response_model=StorableUnitResponse,
+    response_model=StorableUnitResponse | StorableUnitStackedResponse,
     status_code=status.HTTP_200_OK,
     description="Retrieve a StorableUnit by its unique ID. Returns 404 if the StorableUnit does not exist."
 )
-def get_unit(id: int, db: Session = Depends(get_db)):
-    storable_unit = storable_unit_crud.get(db, id)
+def get_unit(id: int, stacked: bool = Query(False), db: Session = Depends(get_db)):
+    storable_unit = storable_unit_crud.get(db, id, stacked)
     if storable_unit is None:
         raise HTTPException(status_code=404, detail=f"StorableUnit with id={id} not found")
     return storable_unit
@@ -31,20 +31,26 @@ def get_unit(id: int, db: Session = Depends(get_db)):
 
 @storable_unit_router.get(
     "/",
-    response_model=PaginationResponse[StorableUnitResponse],
+    response_model=PaginationResponse[StorableUnitResponse] | PaginationResponse[StorableUnitStackedResponse],
     status_code=status.HTTP_200_OK,
     description="Retrieve a list of StorableUnits. Supports pagination with cursor and limit."
 )
 def get_many_units(
-    Cursor: Optional[int] = Query(None, ge=0),
+    cursor: Optional[int] = Query(None, ge=0),
     limit: int = Query(100, ge=1),
+    stacked: bool = Query(False),
     db: Session = Depends(get_db)
 ):
-    storable_units = storable_unit_crud.get_many(db, cursor=Cursor, limit=limit)
-    pk = inspect(StorableUnit).primary_key[0]
-    next_cursor = getattr(storable_units[-1], pk.name) if storable_units and len(storable_units) == limit else None
+    storable_units = storable_unit_crud.get_many(db, cursor, limit, stacked)
+    if not stacked:
+        pk = inspect(StorableUnit).primary_key[0]
+        next_cursor = getattr(storable_units[-1], pk.name) if storable_units and len(storable_units) == limit else None
+        data=list(storable_units)
+    else:
+        data = [StorableUnitStackedResponse(**unit) for unit in storable_units]
+        next_cursor = storable_units[-1].row_num if data and len(data) == limit else None
     return PaginationResponse(
-        data=list(storable_units),
+        data=data,
         next_cursor=next_cursor,
         size=len(storable_units),
         has_more=len(storable_units) == limit
@@ -58,7 +64,7 @@ def get_many_units(
     description="Create a new StorableUnit."
 )
 async def create_unit(obj_in: StorableUnitCreate, db: Session = Depends(get_db)):
-    return await storable_unit_crud.create(db, obj_in)
+    return storable_unit_crud.create(db, obj_in)
 
 
 @storable_unit_router.put(
@@ -85,7 +91,7 @@ def update_unit(id: int, obj_in: StorableUnitUpdate, db: Session = Depends(get_d
     )
 )
 async def consume_unit(id: int, consume_quantity: int = Body(..., gt=0), db: Session = Depends(get_db)):
-    message, storable_unit = await storable_unit_crud.consume(db, id, consume_quantity)
+    message, storable_unit = storable_unit_crud.consume(db, id, consume_quantity)
     return GenericResponse(
         message=message,
         data=StorableUnitResponse.model_validate(storable_unit) if storable_unit else None
--- Shopping and Storage Service/src/services/storable_unit_crud.py
+++ Shopping and Storage Service/src/services/storable_unit_crud.py
@@ -1,39 +1,16 @@
 from fastapi import HTTPException
-from typing import Optional
+from typing import Optional, Sequence, Union, List, Dict, Any
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm import Session
-from sqlalchemy import select
+from sqlalchemy.orm import Session, aliased
+from sqlalchemy import select, func, desc, RowMapping
+from sqlalchemy.inspection import inspect
 from shared.shopping_shared.crud.crud_base import CRUDBase
 from models.storage import StorableUnit, Storage
-from schemas.storable_unit_schemas import StorableUnitCreate, StorableUnitUpdate
-from messaging.producers.ingredient_presence_change_producer import produce_ingredient_presence_change
+from schemas.storable_unit_schemas import StorableUnitCreate, StorableUnitUpdate, StorableUnitStackedResponse
+
 
 class StorableUnitCRUD(CRUDBase[StorableUnit, StorableUnitCreate, StorableUnitUpdate]):
-    async def create(self, db: Session, obj_in: StorableUnitCreate) -> StorableUnit:
-        try:
-            with db.begin():
-                obj_in_data = obj_in.model_dump()
-                db_obj = StorableUnit(**obj_in_data)
-                db.add(db_obj)
-                db.refresh(db_obj)
-
-                if db_obj.component_id is not None:
-                    group_id = db_obj.storage.group_id
-                    same_component_units = db.execute(
-                        select(StorableUnit)
-                        .join(Storage, StorableUnit.storage_id == Storage.storage_id)
-                        .where(
-                            Storage.group_id == group_id,
-                            StorableUnit.unit_name == db_obj.unit_name
-                        )
-                    ).scalars().all()
-                    if len(same_component_units) == 1:
-                        await produce_ingredient_presence_change(group_id=group_id, component_name=db_obj.unit_name, is_present=True)
-        except IntegrityError as e:
-            raise HTTPException(status_code=400, detail=f"Integrity error: {str(e)}")
-        return db_obj
-
-    async def consume(self, db: Session, id: int, consume_quantity: int) -> tuple[str, Optional[StorableUnit]]:
+    def consume(self, db: Session, id: int, consume_quantity: int) -> tuple[str, Optional[StorableUnit]]:
         try:
             with db.begin():
                 unit = db.execute(
@@ -52,18 +29,6 @@ class StorableUnitCRUD(CRUDBase[StorableUnit, StorableUnitCreate, StorableUnitUp
                                f"insufficient quantity (available: {unit.package_quantity}, requested: {consume_quantity})"
                     )
                 elif consume_quantity == unit.package_quantity:
-                    if unit.component_id is not None:
-                        group_id = unit.storage.group_id
-                        same_component_units = db.execute(
-                            select(StorableUnit)
-                            .join(Storage, StorableUnit.storage_id == Storage.storage_id)
-                            .where(
-                                Storage.group_id == group_id,
-                                StorableUnit.unit_name == unit.unit_name
-                            )
-                        ).scalars().all()
-                        if len(same_component_units) == 1:
-                            await produce_ingredient_presence_change(group_id=group_id, component_name=unit.unit_name, is_present=False)
                     db.delete(unit)
                     return "Consumed and deleted", None
                 else:
@@ -71,6 +36,89 @@ class StorableUnitCRUD(CRUDBase[StorableUnit, StorableUnitCreate, StorableUnitUp
                     return "Consumed", unit
         except IntegrityError as e:
             raise HTTPException(status_code=400, detail=f"Integrity error: {str(e)}")
+
+    def get(self, db: Session, id: int, stacked: bool = False) -> Optional[StorableUnit | StorableUnitStackedResponse]:
+        if not stacked:
+            return super().get(db, id)
+        ref1 = aliased(StorableUnit)
+        ref2 = aliased(StorableUnit)
+        unit = db.execute(
+            select(
+                ref1.unit_name,
+                ref1.storage_id,
+                ref1.component_id,
+                ref1.content_type,
+                ref1.content_quantity,
+                ref1.content_unit,
+                func.sum(ref1.package_quantity).label("package_quantity"),
+                func.json_agg(
+                    func.json_build_object(
+                        "unit_id", ref1.unit_id,
+                        "added_date", ref1.added_date,
+                        "expiration_date", ref1.expiration_date,
+                    ).order_by(ref1.added_date)
+                ).label("batch"),
+            )
+            .join(
+                ref2,
+                (ref2.storage_id == ref1.storage_id)
+                & (ref2.unit_name == ref1.unit_name)
+                & (ref2.component_id.is_not_distinct_from(ref1.component_id))                       # type: ignore
+                & (ref2.content_quantity.is_not_distinct_from(ref1.content_quantity))               # type: ignore
+                & (ref2.content_unit.is_not_distinct_from(ref1.content_unit))                       # type: ignore
+            )
+            .group_by(
+                ref1.unit_name,
+                ref1.storage_id,                                                            # type: ignore
+                ref1.component_id,                                                                  # type: ignore
+                ref1.content_type,
+                ref1.content_quantity,                                                              # type: ignore
+                ref1.content_unit,
+            )
+        ).mappings().one_or_none()
+        if unit is None:
+            return None
+        return StorableUnitStackedResponse(**unit)
+
+
+    def get_many(self, db: Session, cursor: Optional[int] = None, limit: int = 100, stacked: bool = False)\
+            -> Sequence[StorableUnit | RowMapping]:
+        if not stacked:
+            return super().get_many(db, cursor, limit)
+        ref = aliased(StorableUnit)
+        subq = (
+            select(
+                ref.unit_name,
+                ref.storage_id,
+                ref.component_id,
+                ref.content_type,
+                ref.content_quantity,
+                ref.content_unit,
+                func.sum(ref.package_quantity).label("package_quantity"),
+                func.json_agg(
+                    func.json_build_object(
+                        "unit_id", ref.unit_id,
+                        "added_date", ref.added_date,
+                        "expiration_date", ref.expiration_date,
+                    ).order_by(ref.added_date)
+                ).label("batch"),
+                func.row_number().over(order_by=desc(ref.added_date)).label("row_num")          # type: ignore
+            )
+            .group_by(
+                ref.unit_name,
+                ref1.storage_id,  # type: ignore
+                ref.component_id,  # type: ignore
+                ref.content_type,
+                ref.content_quantity,  # type: ignore
+                ref.content_unit,
+            )
+        ).subquery()
+        stmt = select(subq)
+        if cursor is not None:
+            stmt = stmt.where(subq.c.row_num > cursor)
+        stmt = stmt.limit(limit)
+        return db.execute(stmt).mappings().all()
+
 
 
 
