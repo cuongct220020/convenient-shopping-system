b0844aa8a95a72662cc4cf2efde4c0a8db7f56f5
ThanhIT0410 <trinhthanhit104@gmail.com>
2025-12-24 10:22:53 +0700

    feat: scheduler and task for meal expire check

---
+++ Meal Service/src/tasks/expire_meals_task.py
@@ -0,0 +1,18 @@
+from datetime import date
+from sqlalchemy import update
+from enums.meal_status import MealStatus
+from models.meal import Meal
+from database import get_db
+
+def expire_meals():
+    db = next(get_db())
+    today = date.today()
+
+    with db.begin():
+        db.execute(
+            update(Meal)
+            .where(Meal.meal_status == MealStatus.CREATED, Meal.date < today)
+            .values(meal_status=MealStatus.EXPIRED)
+        )
+
+    db.close()
