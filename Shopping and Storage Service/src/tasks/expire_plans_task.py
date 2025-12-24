ed544d097f2e660cb5f8244cf20a1a4a56cb7400
ThanhIT0410 <trinhthanhit104@gmail.com>
2025-12-24 09:34:27 +0700

    feat: scheduler and task for plan expire check

--- Shopping and Storage Service/src/tasks/expire_plans_task.py
+++ Shopping and Storage Service/src/tasks/expire_plans_task.py
@@ -4,28 +4,24 @@ from enums.plan_status import PlanStatus
 from models.shopping_plan import ShoppingPlan
 from database import get_db
 
-async def expire_plans():
+def expire_plans():
     db = next(get_db())
-    try:
-        now = datetime.now()
-        expired_count = 0
-        
-        with db.begin():
-            plans = db.execute(
-                select(ShoppingPlan)
-                .where(
-                    ShoppingPlan.plan_status.in_([PlanStatus.CREATED, PlanStatus.IN_PROGRESS]),
-                    ShoppingPlan.deadline < now
-                )
-                .with_for_update()
-            ).scalars().all()
-            
-            for plan in plans:
-                plan.plan_status = PlanStatus.EXPIRED
-                expired_count += 1
-    except:
-        db.rollback()
-        raise
-    finally:
-        db.close()
+    now = datetime.now()
+    expired_count = 0
 
+    with db.begin():
+        plans = db.execute(
+            select(ShoppingPlan)
+            .where(
+                ShoppingPlan.plan_status.in_([PlanStatus.CREATED, PlanStatus.IN_PROGRESS]),
+                ShoppingPlan.deadline < now
+            )
+            .with_for_update()
+        ).scalars().all()
+
+        for plan in plans:
+            plan.plan_status = PlanStatus.EXPIRED
+            expired_count += 1
+
+    db.close()
+    return expired_count
