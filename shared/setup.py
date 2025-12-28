5c24c9ff0aebe8b7d56c668a6de94a5a01dcfd6a
ThanhIT0410 <trinhthanhit104@gmail.com>
2025-12-28 17:30:38 +0700

    chore: update requirements dockerfile docker-compose

--- shared/setup.py
+++ shared/setup.py
@@ -8,11 +8,19 @@ setup(
     author='Đặng Tiến Cường',
     author_email='cuong.dt@example.com',
     install_requires=[
-        'pydantic',
-        'SQLAlchemy'
+        'pydantic==2.11.9',
+        'SQLAlchemy==2.0.43'
     ],
     extras_require={
-        'fastapi': ['fastapi'],
-        'sanic': ['sanic']
+        'fastapi': [
+            'fastapi==0.118.0',
+            'anyio==4.11.0',
+            'starlette==0.48.0',
+            'greenlet==3.2.4',
+            'idna==3.10'
+        ],
+        'sanic': [
+            'sanic==24.12.0'
+        ]
     }
 )
