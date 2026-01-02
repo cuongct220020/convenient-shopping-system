2bf08a48ef0b004b7995a28439db21703c676479
Cuongit1-hust <cuongct0902@gmail.com>
2026-01-03 00:01:16 +0700

    chore: clean source code

--- Recipe Service/src/enums/level.py
+++
@@ -1,6 +0,0 @@
-import enum
-
-class Level(str, enum.Enum):
-    EASY = 'Dễ'
-    MEDIUM = 'Trung bình'
-    HARD = 'Khó'
--- Shopping & Storage Service/src/enums/storage_type.py
+++
@@ -1,7 +0,0 @@
-import enum
-
-class StorageType(str, enum.Enum):
-    FRIDGE = "fridge"
-    FREEZER = "freezer"
-    PANTRY = "pantry"
-    BULK_STORAGE = "bulk_storage"
