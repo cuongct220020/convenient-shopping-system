3ed3c08f89e74b0048b3e0d0f7920f3153039431
ThanhIT0410 <trinhthanhit104@gmail.com>
2026-01-07 00:06:02 +0700

    feat: admin authorization for for non-GET requests in recipe service

--- recipe-service/src/core/__init__.py
+++ recipe-service/src/core/__init__.py
@@ -1,5 +1,6 @@
 from .config import settings, Settings
 from .database import engine, Base, get_db, SessionLocal
+from .admin_middleware import AdminMiddleware
 
-__all__ = ["settings", "Settings", "engine", "Base", "get_db", "SessionLocal"]
+__all__ = ["settings", "Settings", "engine", "Base", "get_db", "SessionLocal", "AdminMiddleware"]
 
