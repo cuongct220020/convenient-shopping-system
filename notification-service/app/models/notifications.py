9b83b949e2723b42ca1910cc491f0357ed8d7d45
ThanhIT0410 <trinhthanhit104@gmail.com>
2026-01-12 10:30:23 +0700

    chore: setup Alembic for Notification Service and create initial migration

--- notification-service/app/models/notifications.py
+++ notification-service/app/models/notifications.py
@@ -1,8 +1,9 @@
+import uuid
 from sqlalchemy import Integer, String, Text, Boolean, DateTime
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.dialects.postgresql.json import JSONB
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.sql import func
-from uuid import UUID
 from datetime import datetime
 from shopping_shared.databases.base_model import Base
 
@@ -11,9 +12,9 @@ class Notification(Base):
     __tablename__ = "notifications"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    group_id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), nullable=False, index=True)
+    group_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), nullable=False, index=True)
     group_name: Mapped[str] = mapped_column(String(255), nullable=False)
-    receiver: Mapped[UUID] = mapped_column(UUID(as_uuid=True), nullable=False, index=True)
+    receiver: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), nullable=False, index=True)
     created_at: Mapped[datetime] = mapped_column(DateTime, default=func.now(), nullable=False)
     template_code: Mapped[str] = mapped_column(String(100), nullable=False)
     raw_data: Mapped[dict] = mapped_column(JSONB, nullable=True)
